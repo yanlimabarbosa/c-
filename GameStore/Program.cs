4b18066e74069b2d393fa4dbaaca0ddb4ee05e89
Yan Lima Barbosa <yanbr763@gmail.com>
2025-12-26 20:39:00 -0300

    feat: refactor Program.cs to improve database migration handling

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,7 +1,14 @@
+using GameStore.Data;
 using GameStore.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
